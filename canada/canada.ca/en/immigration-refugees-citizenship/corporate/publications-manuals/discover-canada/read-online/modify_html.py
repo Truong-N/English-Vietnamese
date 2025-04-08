2e93d289ef13cf6cbed8d336dd7a462839533f37
Truong Nguyen <truong36gm@gmail.com>
2025-04-07 21:49:02 -0400

    changed notice.html file

--- canada/canada.ca/en/immigration-refugees-citizenship/corporate/publications-manuals/discover-canada/read-online/modify_html.py
+++ canada/canada.ca/en/immigration-refugees-citizenship/corporate/publications-manuals/discover-canada/read-online/modify_html.py
@@ -1,10 +1,12 @@
 import os
 
-path2 = "read-online/common_files"
+path2 = "English-Vietnamese/canada/canada.ca/en/immigration-refugees-citizenship/corporate/publications-manuals/discover-canada/read-online/common_files"
+path3 = "English-Vietnamese/canada/canada.ca/en/immigration-refugees-citizenship/corporate/publications-manuals/discover-canada/read-online/"
+
 dir_list2 = os.listdir(path2)
 # for name in dir_list2:
 #     print(name)
-with open("read-online/notice.html", encoding="utf-8") as fr:
+with open(path3 + "notice.html", encoding="utf-8") as fr:
     lines = fr.readlines()
 print(dir_list2[0])
 for line in lines:
@@ -12,5 +14,5 @@ for line in lines:
         if item in line:
             line = line.replace(f"notice_files/{item}", f"common_files/{item}")
             print(line)
-with open("read-online/notice1.html", "w", encoding="utf-8") as fw:
+with open(path3 + "notice.html", "w", encoding="utf-8") as fw:
     fw.writelines(lines)
